c61eecf21dc7f798815cd4b7dcc240b2214c6e12
ahany42 <ahanyfathy42@gmail.com>
2024-08-13 17:43:21 +0200

    Edit Car Initial

--- src/app/Components/card/card.component.ts
+++ src/app/Components/card/card.component.ts
@@ -1,5 +1,6 @@
 import { Component,Input,EventEmitter,Output } from '@angular/core';
 import { CardsListComponent } from '../cards-list/cards-list.component';
+import { Car } from '../Model/car';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
@@ -8,8 +9,12 @@ import { CardsListComponent } from '../cards-list/cards-list.component';
 export class CardComponent {
 @Input() car:any;
 @Output() remove = new EventEmitter<void>();
+// @Output() edit = new EventEmitter<Car>();
+
 DeleteCar(){
   this.remove.emit();
-
 }
+// EditCar(){
+//   this.edit.emit();
+// }
 }
