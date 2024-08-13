a58cafcfc935a7e6584ddaea60b47c373ecf0e24
ahany42 <ahanyfathy42@gmail.com>
2024-08-13 16:18:16 +0200

    Filter Completed

--- src/app/Components/cards-list/cards-list.component.ts
+++ src/app/Components/cards-list/cards-list.component.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class CardsListComponent {
   carsArr:any[]=[];
+  FilterValue="All";
+  filteredCarsArr:any[]=[];
   constructor(){
     this.fetchCars();
   }
@@ -15,9 +17,28 @@ export class CardsListComponent {
       const response = await fetch("https://myfakeapi.com/api/cars");
       const data = await response.json(); 
       this.carsArr = data.cars; 
+      this.filteredCarsArr=this.carsArr;
       console.log(this.carsArr);
     } catch (error) {
       console.error("Error Fetching From Cars Api", error);
     }
+  }
+ 
+  Filter(event:Event){
+   
+      const target = event.target as HTMLInputElement;
+      this.FilterValue=target.value;
+      if(this.FilterValue==="InStock"){
+        this.filteredCarsArr=this.carsArr.filter(car=>car.availability);
+      }
+      else if(this.FilterValue==="OutOfStock")
+        {
+        this.filteredCarsArr=this.carsArr.filter(car=>!car.availability);
+      }
+      else{
+        this.filteredCarsArr=this.carsArr;
+      }
+     
+   
   }
 }
