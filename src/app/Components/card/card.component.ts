a53d065e991a58687e473ac0252b39b3576cccba
Ahany42 <ahanyfathy42@gmail.com>
2024-08-14 21:15:59 +0300

    Edit Card Initial

--- src/app/Components/card/card.component.ts
+++ src/app/Components/card/card.component.ts
@@ -8,13 +8,21 @@ import { CardsListComponent } from '../cards-list/cards-list.component';
 export class CardComponent {
 @Input() car:any;
 @Input() cardViewed: boolean = false;
+@Input() carEdited:boolean = false;
 @Output() remove = new EventEmitter<void>();
 @Output() view = new EventEmitter<any>();
-
+@Output() edit = new EventEmitter<any>();
 DeleteCar(){
   this.remove.emit();
 }
 ViewCar(){
   this.view.emit(this.car);
+}
+checkbox = document.getElementById('AvailableCheckBox') as HTMLInputElement | null;
+if (checkbox:HTMLInputElement) {
+  checkbox.checked =true;
+}
+SaveCard(){
+this.carEdited=false;
 }
 }
